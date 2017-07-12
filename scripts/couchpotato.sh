160bd58d95604c9d673ddcdd677e9845dee77d6c
Lucus Crawford <lucusc@gmail.com>
2017-07-11 20:38:32 -0600

    cleaned up a lot of apps and containers I wont use. Removed the sh scripts since I use docker

--- scripts/couchpotato.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d --name="couchpotato" -v `pwd`/couchpotato:/config --volumes-from datastore --link sabnzbd:sabnzbd --link plex:plex -v /etc/localtime:/etc/localtime:ro -e EDGE=1 -p 5050:5050 funkypenguin/couchpotato
--- scripts/nzbdrone.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d --name="nzbdrone" -v `pwd`/nzbdrone:/config --volumes-from datastore --link sabnzbd:sabnzbd --link plex:plex -v /etc/localtime:/etc/localtime:ro -p 8989:8989 funkypenguin/nzbdrone
